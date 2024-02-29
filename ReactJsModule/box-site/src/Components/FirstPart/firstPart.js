ffcb57d314df13cf1f30e4372b8d4b1b284df58e
Abulqasim <abulqasim.nazarov@gmail.com>
2024-02-29 23:06:12 +0400

    Finish First Block

--- ReactJsModule/box-site/src/Components/FirstPart/firstPart.js
+++ ReactJsModule/box-site/src/Components/FirstPart/firstPart.js
@@ -7,6 +7,12 @@ import './firstPartStyle.scss'
 export const FirstPart = () => {
     return (
         <div className="background-picture">
+            <div className="first-block-inner">
+                <div className="fisrt-block-description">
+                    Building things is our mission.
+
+                </div>
+            </div>
             
         </div>
     )
